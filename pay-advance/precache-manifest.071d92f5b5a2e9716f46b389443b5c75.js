391c50ff707f2c92f7a8d1449e061f2b8878010c
Obelix <obelix@Affinityes_Akl.com>
2020-12-03 14:21:25 +1300

    FrontEnd Artifacts for test deployment 6963

---
+++ pay-advance/precache-manifest.071d92f5b5a2e9716f46b389443b5c75.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1549a75ba39793fe90e9",
+    "url": "/css/about.css"
+  },
+  {
+    "revision": "31c3fd6cb267f127ac69",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "4c0e3d3df050105080cf",
+    "url": "/css/chunk-vendors.css"
+  },
+  {
+    "revision": "a15acc140be57367f89b13ea5748f988",
+    "url": "/img/bi_arrow-left.svg"
+  },
+  {
+    "revision": "3763f6538dd9701259dc9e62fa6fd0dc",
+    "url": "/img/bi_arrow-right.svg"
+  },
+  {
+    "revision": "6db3a0af31c1674b1970d78c8d79a97a",
+    "url": "/img/calendar.svg"
+  },
+  {
+    "revision": "5a477e1dc5164c8d41a3a8815b8f3168",
+    "url": "/img/carbon_calculator-check.svg"
+  },
+  {
+    "revision": "190cc2a87f1b6d58f4a831e3d415aa91",
+    "url": "/img/carbon_chart-custom.svg"
+  },
+  {
+    "revision": "a2e074abcea6eb8694d05dee13dc05dc",
+    "url": "/img/card-tick.svg"
+  },
+  {
+    "revision": "3f44da59600407e617655bd1a96d62df",
+    "url": "/img/clock.svg"
+  },
+  {
+    "revision": "c00d6b03d632465a2a93feffb05389c8",
+    "url": "/img/gold-tick.svg"
+  },
+  {
+    "revision": "1af6090a7968bbd3996f18622ef25e57",
+    "url": "/img/history-item-placeholder.svg"
+  },
+  {
+    "revision": "3dd700094d50471511d1caf888971158",
+    "url": "/img/info-dark.svg"
+  },
+  {
+    "revision": "997ed9896defc35b8ff023b8de05c28e",
+    "url": "/img/info.svg"
+  },
+  {
+    "revision": "faa85fe735086b35c3703ca93327e7c3",
+    "url": "/img/success-tick.svg"
+  },
+  {
+    "revision": "0204150d32ef92e09fb83ebbb46d8a36",
+    "url": "/img/tick-box-payment-requested.svg"
+  },
+  {
+    "revision": "a92f6c4ce03d3fe76e944fe0ab526e4c",
+    "url": "/img/tick-card.svg"
+  },
+  {
+    "revision": "297eccd71c63583a0a6de0ecee79067b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1549a75ba39793fe90e9",
+    "url": "/js/about.js"
+  },
+  {
+    "revision": "31c3fd6cb267f127ac69",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "4c0e3d3df050105080cf",
+    "url": "/js/chunk-vendors.js"
+  },
+  {
+    "revision": "c32d1818e0b8f53bb3b3af4210722e2b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
